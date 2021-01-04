bd508a7e2d85d48547ad75235fa7447c0ecbdacb
Holman Sneyder <u1201569@unimilitar.edu.co>
2021-01-03 20:07:25 -0500

    gradle jar

---
+++ src/test/java/com/segurosbolivar/automation/TestClass.java
@@ -0,0 +1,18 @@
+package com.segurosbolivar.automation;
+
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+import org.testng.collections.Lists;
+
+import java.util.List;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        TestNG testng = new TestNG();
+        List suites = Lists.newArrayList();
+        suites.add("src/test/java/resources/suite.xml");
+        testng.setTestSuites(suites);
+        testng.run();
+    }
+}

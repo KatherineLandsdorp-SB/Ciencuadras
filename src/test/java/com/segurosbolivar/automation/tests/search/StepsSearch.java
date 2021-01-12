a2aa9db7ce9377b6dfc4595d43f11e99726c0879
Holman Sneyder <u1201569@unimilitar.edu.co>
2021-01-12 10:20:41 -0500

    save

--- src/test/java/com/segurosbolivar/automation/tests/search/StepsSearch.java
+++ src/test/java/com/segurosbolivar/automation/tests/search/StepsSearch.java
@@ -9,25 +9,21 @@ public class StepsSearch extends BaseTest {
 
     @Step("The user clicks on the login Link in the Header")
     public StepsSearch clickEntry(){
-        MethodsRegister.clickOnEntry();
         return this;
     }
 
     @Step("The user clicks on the register Link in the Popup")
     public StepsSearch clickAllied(){
-        MethodsRegister.registerAllied();
         return this;
     }
 
     @Step("The user fill all the required fields")
     public StepsSearch fillAll(){
-        MethodsRegister.fillAllTheRequiredFields();
         return this;
     }
 
     @Step("Valid send email")
     public StepsSearch validateEmail(){
-        MethodsRegister.validateRegistration();
         return this;
     }
 

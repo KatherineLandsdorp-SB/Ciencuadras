86521d25c473e0d2dace4a2909afaad8783caef0
Holman Sneyder <u1201569@unimilitar.edu.co>
2021-01-16 22:10:49 -0500

    shared methods

--- src/test/java/com/segurosbolivar/automation/tests/shared/SharedMethods.java
+++ src/test/java/com/segurosbolivar/automation/tests/shared/SharedMethods.java
@@ -9,24 +9,9 @@ import org.openqa.selenium.By;
 public class SharedMethods extends BaseTest {
     protected DriverFacade webDriverFacade;
 
-    public String validateUsernameLoged(){
-
-        String entity = StringUtils.capitalize(services.getField("firstName"))+" "
-                +StringUtils.capitalize(services.getField("secondName"))+" "
-                +StringUtils.capitalize(services.getField("lastName"))+" "
-                +StringUtils.capitalize(services.getField("secondLastName"));
-
-        return driverFacade.getWebDriver().findElement(By.xpath("//*[contains(text(), "+entity+")]")).getText();
-    }
-
     public void clickOnEntry(){
         webDriverFacade.waitForVisibilityOfElement(entryUser);
         entryUser.click();
-    }
-
-    public void loginPerson(){
-        webDriverFacade.waitForVisibilityOfElement(loginPerson);
-        loginPerson.click();
     }
 
     public void clickOnPublish() {

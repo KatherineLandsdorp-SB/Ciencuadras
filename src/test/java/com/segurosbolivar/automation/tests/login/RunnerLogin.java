302a1a6e252df9c2184cfa3db72c3e4045508cf4
holman <u1201569@unimilitar.edu.co>
2021-02-17 13:00:11 -0500

    runner

--- src/test/java/com/segurosbolivar/automation/tests/login/RunnerLogin.java
+++ src/test/java/com/segurosbolivar/automation/tests/login/RunnerLogin.java
@@ -34,35 +34,4 @@ public class RunnerLogin extends Hooks {
                 .clickRemember()
                 .fillAll();
     }
-
-//    @Test(priority=2, groups = { "smoke" })
-//    public void successfullLoginFacebook(){
-//        steps.get()
-//                .clickEntry()
-//                .clickLoginPerson()
-//                .fillAllFacebook();
-//    }
-//
-//    @Test(priority=3)
-//    public void successfullGmail(){
-//        steps.get()
-//                .clickEntry()
-//                .clickLoginPerson()
-//                .fillAllGmail();
-//    }
-
-    // se agrega inmplementaciòn trayendo datos desde servicio de firebase
-   /* ConnectionBD objBd = new ConnectionBD();
-    @Test
-    public void successfulLogin(){
-        System.out.println("Prueba de extraccion de BD " + objBd.getDataService("cienCuadras","username"));
-        steps.get().clickEntry().fillAll("AUTOMATION PRUEBAS AUTOMATION PRUEBAS");
-    }
-*/
-
-
-//    @Test
-//    public void successfulLoginSucess(){
-//        steps.get().clickEntry();
-//    }
 }

207f32a7c63cc3da8200a0a3562e19ac2f1d5bca
German Rojas <german.rojas@segurosbolivar.com>
2021-06-25 15:28:05 -0500

    Se bajan timepos de espera de algunos elementos

--- src/test/java/com/segurosbolivar/automation/commonslocal/layout/Filters.java
+++ src/test/java/com/segurosbolivar/automation/commonslocal/layout/Filters.java
@@ -74,9 +74,9 @@ public class Filters {
     public String clickOnMarkerResultMap() {
 
         String text = "";
-        boolean isPresent = this.methods.visibleElement("mapCanva", 120);
+        boolean isPresent = this.methods.visibleElement("mapCanva", 30);
         if (isPresent) {
-            this.methods.waitingForElement("markerMapResult", 20);
+            this.methods.waitingForElement("markerMapResult", 10);
             this.methods.clickElementJs("markerMapResult");
             this.methods.waitingForElement("tittleInfoMarker", 5);
             text = this.methods.getEntity("tittleInfoMarker").getText();
